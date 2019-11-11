984486377146fa076a3d1ab9c1fed32008c5e721
Joswells <wagnerjr300@gmail.com>
2019-11-11 17:08:05 -0300

    idade concertado

--- main.js
+++ main.js
@@ -10,10 +10,20 @@ $('#modal-excluir').on('show.bs.modal', function (event) {
 
 const calcularIdade = () => {
     let dataNascimento = document.getElementById('nascimento').value;
-    dataNascimento = new Date(dataNascimento);
-    const hoje = new Date();
-    const idade = hoje.getFullYear() - dataNascimento.getFullYear();
-    let idadeLabel = document.getElementById('idade');
-    idadeLabel.innerHTML = 'Idade: ' + idade + ' anos';
+    if (dataNascimento !== "") {
+        dataNascimento = new Date(dataNascimento);
+        const hoje = new Date();
+        let idade = hoje.getFullYear() - dataNascimento.getFullYear();
+        if (hoje.getMonth() < dataNascimento.getMonth()) {
+            idade--;
+            console.log(idade);
+        } else if (hoje.getMonth() == dataNascimento.getMonth() && hoje.getDay() <= dataNascimento.getDay()) {
+            idade--;
+        } else {
+            idade;
+        }
+        let idadeLabel = document.getElementById('idade');
+        idadeLabel.innerHTML = 'Idade: ' + idade + ' anos';
+    }
 
 }
